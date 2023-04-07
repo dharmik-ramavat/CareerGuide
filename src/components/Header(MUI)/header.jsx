bd6efcbab28eedfdc58e38b11a8574d521dfd8eb
srk187 <skadvani653@rku.ac.in>
2023-04-07 14:45:25 +0530

    engineering

--- src/components/Header(MUI)/header.jsx
+++ src/components/Header(MUI)/header.jsx
@@ -8,6 +8,10 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import SearchIcon from "@mui/icons-material/Search";
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&family=Open+Sans&display=swap"
+/>;
 import {
   Grid,
   Paper,
@@ -17,11 +21,14 @@ import {
   InputBase,
   createTheme,
   useMediaQuery,
+  Button,
 } from "@mui/material";
 import logo from "../../assets/career_path.png";
 import { ThemeProvider } from "@emotion/react";
 import DrawerComp from "./DrawerComp";
 import MainContent from "../MainSection/MainContent";
+import { EngineeringCollages } from "../Engineering/EngineeringCollages";
+import { SideBar } from "./Sidebar";
 
 const drawerWidth = 240;
 const navItems = [
@@ -42,11 +49,19 @@ const theme = createTheme({
       main: "#00897B",
       contrastText: "#fff",
     },
+    secondary: {
+      main: "#fff",
+    },
   },
 });
 
 function DrawerAppBar(props) {
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  const [value, setValue] = React.useState(0);
+  function handlechange(event, val) {
+    console.warn(val);
+    setValue(val);
+  }
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -57,12 +72,11 @@ function DrawerAppBar(props) {
             {isMatch ? (
               <>
                 <DrawerComp />
+
                 <img src={logo} height={100} alt="Career Path" />
               </>
             ) : (
               <>
-                
-
                 <img src={logo} height={100} alt="Career Path" />
 
                 <Box
@@ -102,8 +116,8 @@ function DrawerAppBar(props) {
                       >
                         <InputBase
                           sx={{ ml: 1, flex: 1 }}
-                          placeholder="Search Google Maps"
-                          inputProps={{ "aria-label": "search google maps" }}
+                          placeholder="Search Collages"
+                          inputProps={{ "aria-label": "search collages" }}
                         />
                         <Divider
                           sx={{ height: 28, m: 0.5 }}
@@ -127,15 +141,51 @@ function DrawerAppBar(props) {
                           justifyContent: "center",
                         }}
                       >
-                        <Tabs indicatorColor="#fff" textColor="#fff ">
-                          {navItems.map((item) => (
+                        {/* <Tabs
+                          value={0}
+                          onChange={handlechange}
+                          indicatorColor="#fff"
+                          sx={{ fontWeight: "bold" }}
+                          textColor="#000 "
+                        >
+                          {/* {navItems.map((item) => (
                             <Tab label={item} />
+                            
+                          ))} */}
+                        {/* <Tab label="Home" />
+                          <Tab label="About" />
+                          <Tab label="Contact" />
+                          <Tab label="After 10th" />
+                          <Tab label="Diploma" />
+                          <Tab label="After 12th" />
+                          <Tab label="Engineering" />
+                          <Tab label="B.COM" />
+                          <Tab label="BSC" />
+                          <Tab label="MBA" />
+                        </Tabs>  */}
+                        <Tabs
+                          value={value} // use the value state here
+                          sx={{ fontWeight: "bold" }}
+                          textColor="#000"
+                          onChange={handlechange} // add onChange event handler
+                        >
+                          {navItems.map((item, index) => (
+                            <Tab key={index} label={item} value={index} /> // add value prop
                           ))}
                         </Tabs>
                       </Box>
                     </Grid>
                   </Grid>
                 </Box>
+
+                <Button
+                  color="secondary"
+                  sx={{ fontFamily: "Open Sans" }}
+                  size="large"
+                  variant="contained"
+                >
+                  Login
+                </Button>
               </>
             )}
           </Toolbar>
@@ -144,7 +194,29 @@ function DrawerAppBar(props) {
 
       <Box component="main" sx={{ p: 3 }}>
         <Toolbar />
-        <MainContent/>
+        {value == 0 ? (
+          <MainContent />
+        ) : value == 1 ? (
+          <h1>About</h1>
+        ) : value == 2 ? (
+          <h1>Contact Us</h1>
+        ) : value == 3 ? (
+          <h1>After 10th</h1>
+        ) : value == 4 ? (
+          <h1>Diploma</h1>
+        ) : value == 5 ? (
+          <h1>After 12th</h1>
+        ) : value == 6 ? (
+          <SideBar/>
+        ) : value == 7 ? (
+          <h1>Diploma</h1>
+        ) : value == 8 ? (
+          <h1>BSC</h1>
+        ) : (
+          <MainContent/>
+        )}
+        {/* <MainContent />
+        <EngineeringCollages /> */}
       </Box>
     </Box>
   );
